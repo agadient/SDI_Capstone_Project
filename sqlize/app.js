28e3179cb6ac137329dd86f83f71e162ec6b024c
agadient <agadient@mit.edu>
2021-01-05 10:47:13 -0700

    add docker files and changes to connect to database. start keycloak

--- sqlize/app.js
+++ sqlize/app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const keycloak = require("./config/keycloak").initKeycloak();
+const cors = require('cors')
 // Set up the express app
 const app = express();
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+/*app.use(keycloak.middleware(
+  { admin: '/auth'}
+))*/
+app.use(cors({origin: 'http://localhost:8000'}));
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 require('./routes')(app);
-app.get('*', (req, res) => res.status(200).send({
+app.get('/', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
+
+app.get('/auth', (req, res)=> res.status(200).send({message: "Hi!"}))
 
 module.exports = app;
