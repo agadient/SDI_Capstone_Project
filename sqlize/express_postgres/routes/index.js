54f8596b42f14e6c887e0c9c00b080b27f5af113
agadient <agadient@mit.edu>
2021-01-06 09:48:29 -0700

    add gitignore and minor changes

--- sqlize/express_postgres/routes/index.js
+++ sqlize/express_postgres/routes/index.js
@@ -2,7 +2,7 @@ const dataController = require('../controllers').data;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
-    message: 'Welcome to the Todos API!',
+    message: 'Welcome to the MYSQL API!',
   }));
 
   app.post('/api/data', dataController.create);
