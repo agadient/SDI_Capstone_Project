fb0f146a3334d93d71258d3972d7de0cc9732eb4
agadient <agadient@mit.edu>
2021-01-08 14:52:25 -0700

    add cors changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,11 +10,24 @@ export default function App(){
    const handleChange = (event) => {
      setStatus(prev => ({...prev, [event.target.name]: event.target.value}));
 
-   }
-  
+   }  
 
-  const writeDatabase = (event) => {
-    event.preventDefault()
+  const writeDatabase = async (event) => {
+    console.log("sending data")
+    let response = await fetch("http://localhost:8000/writeDB", { 
+      // Adding method type 
+      method: "POST", 
+      // Adding body or contents to send 
+      body: JSON.stringify({ 
+          title: status.title
+      }), 
+      // Adding headers to the request 
+      headers: { 
+          "Content-type": "application/json; charset=UTF-8"
+      } 
+  })
+    let data = await response.data
+    console.log(data)
   }
 
   const readDatabase = (event) => {
@@ -24,19 +37,18 @@ export default function App(){
   const sendToken = (event) => {
     event.preventDefault()
   }    
-
   
-    return (
-      <div>
-        Input data to stick in database: <input name="sampledata" className="sampledata" type="text" onChange={(event)=> handleChange(event)}></input><button onSubmit={writeDatabase}>Send Data to Database</button>
-        <br/>
-        Input encoded JWT token to parse: <textarea name="token" type="text" onChange={(event)=> handleChange(event)}></textarea><button onSubmit={sendToken}>Send Data to Database</button>
-        <br/>
-        <button onSubmit={readDatabase}>Read data from database</button>
-        <h1>DISPLAY</h1>
-        <textarea type="text" readOnly>{tokenData}</textarea>
-      </div>
-    );
+  return (
+    <div>
+      Input data to stick in database: <input name="title" className="title" type="text" onChange={(event)=> handleChange(event)}></input><button onClick={writeDatabase}>Send Data to Database</button>
+      <br/>
+      Input encoded JWT token to parse: <textarea name="token" type="text" onChange={(event)=> handleChange(event)}></textarea><button onSubmit={sendToken}>Send Data to Database</button>
+      <br/>
+      <button onSubmit={readDatabase}>Read data from database</button>
+      <h1>DISPLAY</h1>
+      <textarea type="text" readOnly>{tokenData}</textarea>
+    </div>
+  );
   
 }
 
